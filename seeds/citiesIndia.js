18b6867a5b67d4609392a7bf47cd9cd407463cda
siddharthgoel94 <siddharth_goel2105@outlook.com>
2023-06-18 02:34:01 +0530

    updated project

---
+++ seeds/citiesIndia.js
@@ -0,0 +1,247 @@
+[
+    {
+    city: "Udaipur",
+    growth_from_2000_to_2013: "37.2%",
+    latitude: 24.5854,
+    longitude: 73.7125,
+    population: "474531",
+    rank: "64",
+    state: "Rajasthan"
+    },
+    {
+    city: "Varanasi",
+    growth_from_2000_to_2013: "32.3%",
+    latitude: 25.3176,
+    longitude: 82.9739,
+    population: "1198491",
+    rank: "13",
+    state: "Uttar Pradesh"
+    },
+    {
+    city: "Agra",
+    growth_from_2000_to_2013: "46.0%",
+    latitude: 27.1767,
+    longitude: 78.0081,
+    population: "1699194",
+    rank: "15",
+    state: "Uttar Pradesh"
+    },
+    {
+    city: "Jaipur",
+    growth_from_2000_to_2013: "39.8%",
+    latitude: 26.9124,
+    longitude: 75.7873,
+    population: "3073350",
+    rank: "9",
+    state: "Rajasthan"
+    },
+    {
+    city: "Mysuru",
+    growth_from_2000_to_2013: "22.9%",
+    latitude: 12.2958,
+    longitude: 76.6394,
+    population: "990900",
+    rank: "29",
+    state: "Karnataka"
+    },
+    {
+    city: "Goa",
+    growth_from_2000_to_2013: "14.9%",
+    latitude: 15.2993,
+    longitude: 74.1240,
+    population: "1458545",
+    rank: "7",
+    state: "Goa"
+    },
+    {
+    city: "Kochi",
+    growth_from_2000_to_2013: "5.1%",
+    latitude: 9.9312,
+    longitude: 76.2673,
+    population: "677381",
+    rank: "48",
+    state: "Kerala"
+    },
+    {
+    city: "Amritsar",
+    growth_from_2000_to_2013: "15.1%",
+    latitude: 31.6339,
+    longitude: 74.8723,
+    population: "1132761",
+    rank: "23",
+    state: "Punjab"
+    },
+    {
+    city: "Pushkar",
+    growth_from_2000_to_2013: "13.3%",
+    latitude: 26.4898,
+    longitude: 74.5510,
+    population: "21116",
+    rank: "NaN",
+    state: "Rajasthan"
+    },
+    {
+    city: "Shimla",
+    growth_from_2000_to_2013: "12.1%",
+    latitude: 31.1048,
+    longitude: 77.1734,
+    population: "171817",
+    rank: "55",
+    state: "Himachal Pradesh"
+    },
+    {
+    city: "Ooty",
+    growth_from_2000_to_2013: "17.9%",
+    latitude: 11.4102,
+    longitude: 76.6952,
+    population: "88480",
+    rank: "NaN",
+    state: "Tamil Nadu"
+    },
+    {
+    city: "Puri",
+    growth_from_2000_to_2013: "24.5%",
+    latitude: 19.8134,
+    longitude: 85.8315,
+    population: "201026",
+    rank: "NaN",
+    state: "Odisha"
+    },
+    {
+    city: "Darjeeling",
+    growth_from_2000_to_2013: "16.1%",
+    latitude: 27.0360,
+    longitude: 88.2627,
+    population: "118805",
+    rank: "NaN",
+    state: "West Bengal"
+    },
+    {
+    city: "Ajmer",
+    growth_from_2000_to_2013: "20.1%",
+    latitude: 26.4499,
+    longitude: 74.6399,
+    population: "542580",
+    rank: "NaN",
+    state: "Rajasthan"
+    },
+    {
+    city: "Gangtok",
+    growth_from_2000_to_2013: "12.9%",
+    latitude: 27.3389,
+    longitude: 88.6065,
+    population: "98434",
+    rank: "NaN",
+    state: "Sikkim"
+    },
+    {
+    city: "Munnar",
+    growth_from_2000_to_2013: "16.9%",
+    latitude: 10.0889,
+    longitude: 77.0595,
+    population: "55195",
+    rank: "NaN",
+    state: "Kerala"
+    },
+    {
+    city: "Mahabalipuram",
+    growth_from_2000_to_2013: "13.2%",
+    latitude: 12.6269,
+    longitude: 80.1926,
+    population: "12513",
+    rank: "NaN",
+    state: "Tamil Nadu"
+    },
+    {
+    city: "Rishikesh",
+    growth_from_2000_to_2013: "13.1%",
+    latitude: 30.0869,
+    longitude: 78.2676,
+    population: "102138",
+    rank: "NaN",
+    state: "Uttarakhand"
+    },
+    {
+    city: "Jaisalmer",
+    growth_from_2000_to_2013: "24.5%",
+    latitude: 26.9157,
+    longitude: 70.9083,
+    population: "65579",
+    rank: "NaN",
+    state: "Rajasthan"
+    },
+    {
+    city: "Kanyakumari",
+    growth_from_2000_to_2013: "15.6%",
+    latitude: 8.0883,
+    longitude: 77.5385,
+    population: "22257",
+    rank: "NaN",
+    state: "Tamil Nadu"
+    },
+    {
+    city: "Mamallapuram",
+    growth_from_2000_to_2013: "12.2%",
+    latitude: 12.6269,
+    longitude: 80.1926,
+    population: "12513",
+    rank: "NaN",
+    state: "Tamil Nadu"
+    },
+    {
+    city: "Kodaikanal",
+    growth_from_2000_to_2013: "11.5%",
+    latitude: 10.2381,
+    longitude: 77.4892,
+    population: "36912",
+    rank: "NaN",
+    state: "Tamil Nadu"
+    },
+    {
+    city: "Pondicherry",
+    growth_from_2000_to_2013: "28.5%",
+    latitude: 11.9139,
+    longitude: 79.8145,
+    population: "67549",
+    rank: "NaN",
+    state: "Puducherry"
+    },
+    {
+    city: "Nainital",
+    growth_from_2000_to_2013: "18.7%",
+    latitude: 29.3807,
+    longitude: 79.4636,
+    population: "41082",
+    rank: "NaN",
+    state: "Uttarakhand"
+    },
+    {
+    city: "Kolkata",
+    growth_from_2000_to_2013: "6.6%",
+    latitude: 22.5726,
+    longitude: 88.3639,
+    population: "1497401",
+    rank: "3",
+    state: "West Bengal"
+    },
+    {
+    city: "Kovalam",
+    growth_from_2000_to_2013: "16.2%",
+    latitude: 8.4004,
+    longitude: 76.9780,
+    population: "18747",
+    rank: "NaN",
+    state: "Kerala"
+    },
+    {
+    city: "Kumarakom",
+    growth_from_2000_to_2013: "10.6%",
+    latitude: 9.6094,
+    longitude: 76.4212,
+    population: "6197",
+    rank: "NaN",
+    state: "Kerala"
+    }
+    ]
+    
+    
--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,126 @@
+const indiaCities=[
+    {
+      "name": "Kanpur",
+      "latitude": 26.4499,
+      "longitude": 80.3319
+    },
+    {
+      "name": "Visakhapatnam",
+      "latitude": 17.6868,
+      "longitude": 83.2185
+    },
+    {
+      "name": "Nagpur",
+      "latitude": 21.1458,
+      "longitude": 79.0882
+    },
+    {
+      "name": "Indore",
+      "latitude": 22.7196,
+      "longitude": 75.8577
+    },
+    {
+      "name": "Coimbatore",
+      "latitude": 11.0168,
+      "longitude": 76.9558
+    },
+    {
+      "name": "Patna",
+      "latitude": 25.5941,
+      "longitude": 85.1376
+    },
+    {
+      "name": "Vadodara",
+      "latitude": 22.3072,
+      "longitude": 73.1812
+    },
+    {
+      "name": "Bhubaneswar",
+      "latitude": 20.2961,
+      "longitude": 85.8245
+    },
+    {
+      "name": "Thiruvananthapuram",
+      "latitude": 8.5241,
+      "longitude": 76.9366
+    },
+    {
+      "name": "Guwahati",
+      "latitude": 26.1445,
+      "longitude": 91.7362
+    },
+    {
+      "name": "Mangalore",
+      "latitude": 12.9141,
+      "longitude": 74.8560
+    },
+    {
+      "name": "Raipur",
+      "latitude": 21.2514,
+      "longitude": 81.6296
+    },
+    {
+      "name": "Ranchi",
+      "latitude": 23.3441,
+      "longitude": 85.3096
+    },
+    {
+      "name": "Amravati",
+      "latitude": 20.9374,
+      "longitude": 77.7796
+    },
+    {
+      "name": "Jodhpur",
+      "latitude": 26.2389,
+      "longitude": 73.0243
+    },
+    {
+      "name": "Dehradun",
+      "latitude": 30.3165,
+      "longitude": 78.0322
+    },
+    {
+      "name": "Srinagar",
+      "latitude": 34.0837,
+      "longitude": 74.7973
+    },
+    {
+      "name": "Kozhikode",
+      "latitude": 11.2588,
+      "longitude": 75.7804
+    },
+    {
+      "name": "Jamshedpur",
+      "latitude": 22.8046,
+      "longitude": 86.2029
+    },
+    {
+      "name": "Allahabad",
+      "latitude": 25.4358,
+      "longitude": 81.8463
+    },
+    {
+        "name": "Jaipur",
+        "latitude": 26.9124,
+        "longitude": 75.7873
+      },
+      {
+        "name": "Lucknow",
+        "latitude": 26.8467,
+        "longitude": 80.9462
+      },
+      {
+        "name": "Chandigarh",
+        "latitude": 30.7333,
+        "longitude": 76.7794
+      },
+      {
+        "name": "Agra",
+        "latitude": 27.1767,
+        "longitude": 78.0081
+      }
+  ]
+  
 descriptors = [
     'Forest',
     'Ancient',
@@ -75,21 +198,25 @@ const seedDb=async ()=>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000=Math.floor(Math.random()*1000);
+        const random20=Math.floor(Math.random()*20);
         const calcPrice=Math.floor(Math.random()*20)+10;
-        const newCamp=new Campground({location:`${cities[random1000].city}, ${cities[random1000].state}`,
+        const newCamp=new Campground({location:`${indiaCities[random20].name}`,
         title:`${makeName(descriptors)} ${makeName(places)}`,
         author:"6484228b1ce86223ac5d7285",
         price:calcPrice,
-        geometry: { type: 'Point', coordinates: [cities[random1000].longitude,cities[random1000].latitude] },
+        geometry: { type: 'Point', 
+        // coordinates: [cities[random1000].longitude,cities[random1000].latitude]
+        coordinates: [indiaCities[random20].longitude,indiaCities[random20].latitude]
+     },
         images: [
             {
-              url: 'https://res.cloudinary.com/dsslq2vi3/image/upload/v1686376919/YelpCamp/xzr3igl66cimealn5hk3.jpg',
-              name: 'YelpCamp/xzr3igl66cimealn5hk3',
+              url: 'https://res.cloudinary.com/dsslq2vi3/image/upload/v1686920909/YelpCamp/ss8vzkxw2yk2oielq9fv.jpg',
+              name: 'YelpCamp/ss8vzkxw2yk2oielq9fv',
               
             },
             {
-              url:'https://res.cloudinary.com/dsslq2vi3/image/upload/v1686376921/YelpCamp/g6rjpivxmyqbiaplmd5d.jpg' ,
-              name:'YelpCamp/g6rjpivxmyqbiaplmd5d',
+              url:'https://res.cloudinary.com/dsslq2vi3/image/upload/v1686920907/YelpCamp/wj9s3g990ugwyqdslaja.jpg' ,
+              name:'YelpCamp/wj9s3g990ugwyqdslaja',
              
             }
           ],
